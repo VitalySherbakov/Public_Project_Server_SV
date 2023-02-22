5c8b1aae18f255651c5fea0b7f586d1b4142a7db
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2023-02-22 19:46:13 +0200

    234234

---
+++ Public_SV_Server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Установка и Развертывание Проекта Server SV"
+$distributivelinex=$(lsb_release -is)
+$numberversionlinex=$(lsb_release -rs)
+if [ "$distributivelinex" == "Debian" ]; then
+	echo "Линекс: $distributivelinex"
+	if [ "$numberversionlinex" == 11 ]; then
+	echo "Версия: $numberversionlinex"
+	git clone https://github.com/VitalySherbakov/Public_Project_Server_SV
+	./Public_SV_Server.sh
+	fi
+	if [ "$numberversionlinex" == 10 ]; then
+	echo "Версия: $numberversionlinex"
+	fi
+	if [ "$numberversionlinex" == 9 ]; then
+	echo "Версия: $numberversionlinex"
+	fi
+fi
+if [ "$distributivelinex" == "Ubuntu" ]; then
+	echo "Линекс: $distributivelinex"
+	if [ "$numberversionlinex" == 10 ]; then
+	echo "Версия: $numberversionlinex"
+	fi
+fi
+read -p "Нажмите Enter, чтобы продолжить"
